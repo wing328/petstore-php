5aead9186bba0b09b834a65a6eae67a9dda54b2b
wing328 <wing328hk@gmail.com>
2015-04-06 12:24:39 +0800

    add example

---
+++ examples/test.php
@@ -0,0 +1,19 @@
+<?php
+require_once('vendor/autoload.php');
+//require_once('vendor/SwaggerPetstore/SwaggerPetstore-php/SwaggerPetstore.php');
+
+$tag = new SwaggerPetstore\models\Tag();
+
+// initialize the API client
+$api_client = new SwaggerPetstore\APIClient('http://petstore.swagger.io/v2');
+$petId = 5;  // ID of pet that needs to be fetched
+try {
+    $pet_api = new SwaggerPetstore\PetAPI($api_client);
+    // return Pet (model)
+    $response = $pet_api->getPetById($petId);
+    var_dump($response);
+} catch (Exception $e) {
+    echo 'Caught exception: ', $e->getMessage(), "\n";
+}
+
+?>
